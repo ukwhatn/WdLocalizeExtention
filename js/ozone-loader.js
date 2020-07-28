3a1b8a5e33ffaacdf4f20e4d883bb2c4fd58a9ec
ukwhatn <ukwhatn@gmail.com>
2020-07-28 22:39:52 +0900

    セミコロン付けた(2)

--- js/ozone-loader.js
+++ js/ozone-loader.js
@@ -1,6 +1,6 @@
 (function () {
     if (document.getElementsByTagName("body")[0]) {
-        console.log("bodyの読み込みが完了しました。ダイアログ日本語化スクリプトを挿入します。")
+        console.log("bodyの読み込みが完了しました。ダイアログ日本語化スクリプトを挿入します。");
         var s, th;
         th = document.getElementsByTagName("body")[0];
         s = document.createElement('script');
@@ -8,7 +8,7 @@
         s.setAttribute('src', chrome.extension.getURL('/js/ozone-script.js'));
         return th.appendChild(s);
     } else {
-        console.log("bodyの読み込みを待っています。")
+        console.log("bodyの読み込みを待っています。");
         return setTimeout(arguments.callee, 1000);
     }
 })();
